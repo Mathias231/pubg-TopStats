fd1033dfec5fee2d10e900bbe01374017f4e4fc8
Mathias231 <mathias.krogh@outlook.com>
2022-08-03 15:10:04 +0200

    First commit

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
 function Home() {
-  return (
+    const url = "https://pubg.op.gg/api/users/59fe361a66d17500012db6bd/ranked-stats?season=pc-2018-18&queue_size=1&mode=fpp";
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        axios.get(url)
+        .then(res => {
+            setData(res.data);
+        })
+    }, [url]);
+
+    if(data) {
+        return(
+            <div>
+                {console.log(data)}
+            </div>
+        )
+    }
+
+    return (
     <div>Home</div>
-  )
+    )
 }
 
 export default Home
