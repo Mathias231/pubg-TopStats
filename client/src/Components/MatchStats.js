79eca7033d66f364fc78c316aa7e0008e05ea357
Mathias231 <mathias.krogh@outlook.com>
2022-08-04 14:44:52 +0200

    first commit

---
+++ client/src/Components/MatchStats.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function MatchStats(props) {
+    const items = props.matches.slice(0, 20)
+    console.log(items)
+
+  return (
+    <div>
+        <h1>w</h1>
+    </div>
+  )
+}
+
+export default MatchStats
