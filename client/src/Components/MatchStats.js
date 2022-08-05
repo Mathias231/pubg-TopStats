01ab8fb12ccbb4c25108fa0bd8e7fc7eca6f8b60
Mathias231 <mathias.krogh@outlook.com>
2022-08-05 10:36:34 +0200

    Fetching matches and will soon display game on webpage

--- client/src/Components/MatchStats.js
+++ client/src/Components/MatchStats.js
@@ -2,27 +2,32 @@ import React from 'react'
 import axios from 'axios'
 
 function MatchStats(props) {
-  var items = props.matches.slice(0, 2);
-  let url = ""
-
+  var items = props.matches.slice(0, 8);
+  var matchId;
+  var data = [];
+  console.log(items)
 
   items.map((items) => {
-    url = items.id
+    matchId = items.id
 
     axios({
       method: 'get',
-      url: "https://api.pubg.com/shards/steam/matches/" + url,
+      url: "https://api.pubg.com/shards/steam/matches/" + matchId,
       headers: {
           "Accept": 'application/vnd.api+json',
       },
     }).then(res => {
-        console.log(res)
+      data.push(res)
     });
+    
+    return data;
   })
+  
+  console.log(data)
 
   return (
     <div>
-        <h1>w</h1>
+        <h1></h1>
     </div>
   )
 }
