5a9a8fb1c0d010c425cb4a13ac7ebcefdeffb786
Mathias231 <mathias.krogh@outlook.com>
2022-08-05 14:22:31 +0200

    Huge fix.. logging out data from a match

--- client/src/Components/MatchStats.js
+++ client/src/Components/MatchStats.js
@@ -1,34 +1,35 @@
-import React from 'react'
+import React, { useLayoutEffect, useRef} from 'react'
 import axios from 'axios'
 
 function MatchStats(props) {
-  var items = props.matches.slice(0, 8);
-  var matchId;
-  var data = [];
-  console.log(items)
-
-  items.map((items) => {
-    matchId = items.id
-
-    axios({
-      method: 'get',
-      url: "https://api.pubg.com/shards/steam/matches/" + matchId,
-      headers: {
-          "Accept": 'application/vnd.api+json',
-      },
-    }).then(res => {
-      data.push(res)
-    });
-    
-    return data;
-  })
+  // Get X data
+  var data = useRef(props.matches.slice(0, 2));
   
-  console.log(data)
+  useLayoutEffect(() => {
+    let matchId;
+    let url;
+
+    data.current.map((data) => {
+      matchId = data.id;
+      url = "https://api.pubg.com/shards/steam/matches/" + matchId;
+
+      axios({
+        method: 'get',
+        url: url,
+        headers: {
+            "Accept": 'application/vnd.api+json',
+        },
+      }).then(res => {
+        console.log(res)
+      });
+
+
+      return true;
+    })
+  }, [])
 
   return (
-    <div>
-        <h1></h1>
-    </div>
+    <div>Error</div>
   )
 }
 
