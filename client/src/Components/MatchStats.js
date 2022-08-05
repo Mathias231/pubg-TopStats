8580fce9a0b859220ccf253849453c6fae133675
Mathias231 <mathias.krogh@outlook.com>
2022-08-05 10:04:05 +0200

    Fetching each match

--- client/src/Components/MatchStats.js
+++ client/src/Components/MatchStats.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import axios from 'axios'
 
 function MatchStats(props) {
-    const items = props.matches.slice(0, 20)
-    console.log(items)
+  var items = props.matches.slice(0, 2);
+  let url = ""
+
+
+  items.map((items) => {
+    url = items.id
+
+    axios({
+      method: 'get',
+      url: "https://api.pubg.com/shards/steam/matches/" + url,
+      headers: {
+          "Accept": 'application/vnd.api+json',
+      },
+    }).then(res => {
+        console.log(res)
+    });
+  })
+
   return (
     <div>
         <h1>w</h1>
