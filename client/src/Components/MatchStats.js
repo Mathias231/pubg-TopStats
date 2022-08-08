680bf7e5e6c62cea0ef29dcd27b2fb6f099e004b
Mathias231 <mathias.krogh@outlook.com>
2022-08-08 15:16:45 +0200

    First commit

--- client/src/Components/MatchStats.js
+++ client/src/Components/MatchStats.js
@@ -1,45 +1,13 @@
-import React, { useState, useLayoutEffect, useRef} from 'react'
-import axios from 'axios'
+import React from 'react'
 
 function MatchStats(props) {
   // Get X data
-  var data = useRef(props.matches.slice(0, 2));
-  const [matchData, setMatchData] = useState([]);
-
-  useLayoutEffect(() => {
-    let matchId;
-    let url;
-
-    data.current.map((data) => {
-      matchId = data.id;
-      url = "https://api.pubg.com/shards/steam/matches/" + matchId;
-      // MAke an array here and push data from .then into this THEN return this array at the bottom 
-
-      axios({
-        method: 'get',
-        url: url,
-        headers: {
-            "Accept": 'application/vnd.api+json',
-        },
-      }).then(res => {
-        console.log(res)
-        console.log(matchId)
-        setMatchData(res.data) // currently puts 1 data into useState hook ??? Why
-      });
-
-      return true;
-    })
-  }, [])
-
-  if(matchData) {
-    return(
-      <div>Correct
-      </div>
-    )
-  }
+console.log(props)
+  const username = props.matches.attributes.name;
+  console.log(username)
 
   return (
-    <div>Error</div>
+    <div>complete</div>
   )
 }
 
