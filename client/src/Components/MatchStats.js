eccc7b1787759e61d3e86b9313a6a14152c4c88f
Mathias231 <mathias.krogh@outlook.com>
2022-08-10 13:01:45 +0200

    It works!! (for real this time) | Calling async function with useEffect and all match data is now able to be used for dispaly

--- client/src/Components/MatchStats.js
+++ client/src/Components/MatchStats.js
@@ -1,28 +1,43 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import axios from 'axios'
 
 function MatchStats(props) {
-  const [data] = useState(props.matches.slice(0, 5))
-  
-  useEffect(() => {
-    const matchList = [];
-    console.log(matchList)
-    data.map((data) => {
-      axios({
+  // Data from parent script and slice all down to 4
+  const [data] = useState(props.matches.slice(0, 10));
+  const [matchList, setMatchList] = useState([]);
+
+  const nut = useCallback(async () => {
+    // Creates an empty array
+    let matchData = [];
+
+    for(let match of data) {
+      await axios({
         method: 'get',
-        url: 'https://api.pubg.com/shards/steam/matches/' + data.id,
+        url: 'https://api.pubg.com/shards/steam/matches/' + match.id,
         headers: {
             "Accept": 'application/vnd.api+json',
         },
-    }).then(res => {
-      matchList.push(res.data)
-    });
-      return true;
-    })
-  }, [data]);
+      }).then(res => {
+        // Pushing response data into matchData array
+        matchData.push(res.data);
+      });
+    }
+    // Updates matchList with matchData
+    setMatchList(matchData)
+  }, [data])
+
+  // Run async function
+  useEffect(() => {
+    nut();
+  }, [nut])
 
   return (
-    <div>MatchStats</div>
+    <div>MatchStats
+      {matchList.map((data) => (
+        <h1 key={data.data.id}>www</h1>
+      ))}
+
+    </div>
   )
 }
 
