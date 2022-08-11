9ec1c58ef61b213c250027f303b6fe4c26808959
Mathias231 <mathias.krogh@outlook.com>
2022-08-11 15:24:34 +0200

    Added (useState) variable with name matchId

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -6,7 +6,9 @@ import MatchStats from '../Components/MatchStats';
 
 function Home() {
     const [user, setUser] = useState(null);
-
+    const [matchId, setMatchId] = useState();
+    console.log(matchId);
+    
     const url = "https://api.pubg.com/shards/steam/players?filter[playerNames]=Bakern999";
     const auth = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI4NzdlMTFmMC1mNWVlLTAxM2EtY2ZmMy0wODM2YzIwNWY4NzgiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNjU5NTk1MzQ3LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6Ii0wNGYyZTljYy1kZmVhLTRhMWYtOWJkZi1lYjE1OGE2M2M0YjcifQ.zBEjFzJ1SRpv0EoM-ut44oqJbm37dPcwTBwFvFRbwro";
 
@@ -34,7 +36,7 @@ function Home() {
 
                 <div className='grid grid-cols-2 mt-1 space-x-4'>
                     <div className="">
-                        <MatchStats matches={user.data[0].relationships.matches.data} />
+                        <MatchStats changeData={(matchId) => setMatchId(matchId)} matches={user.data[0].relationships.matches.data} />
                     </div>
                     <div className="">03</div>
                 </div>
