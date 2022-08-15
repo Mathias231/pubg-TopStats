3eed568a7cd835a89ac5a792a59a1ef9b9e87563
Mathias231 <mathias.krogh@outlook.com>
2022-08-15 13:12:58 +0200

    Fetching data from match

--- client/src/Components/MatchDisplay.js
+++ client/src/Components/MatchDisplay.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
+import axios from 'axios'
 
 function MatchDisplay(props) {
-    console.log(props)
+    let matchId = props.matchId
+    const [matchData, setMatchData] = useState([])
+    
+    const fetchMatchData = useCallback(async () => {
+        axios({
+            method: 'get',
+            url: "https://api.pubg.com/shards/steam/matches/" + matchId,
+            headers: {
+                "Accept": 'application/vnd.api+json'
+            },
+        }).then(res => {
+            setMatchData(res.data);
+        });
+    });
 
+    useEffect(() => {
+        fetchMatchData();
+    }, [matchId])
+    console.log(matchData)
     return (
-        <div>MatchDisplay: {props.matchId}</div>
+        <div>MatchDisplay: {matchId}</div>
     )
 }
 
